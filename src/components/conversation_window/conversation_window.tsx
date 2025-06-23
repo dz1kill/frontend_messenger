8a87b12f6d6c3e3fa84ccefc345b70382e55a1cc
dz1kill <dz1kill06@gmail.com>
2025-06-23 17:25:25 +0300

    add convertasion window

---
+++ src/components/conversation_window/conversation_window.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../../styles/conversation_window.module.css";
+
+const ConversationWindow = () => {
+  return (
+    <div className={styles.conversationWindow}>
+      <div className={styles.conversationHeader}>
+        <h2>Иван Иванов</h2>
+      </div>
+
+      <div className={styles.messagesContainer}></div>
+
+      <div className={styles.messageInput}>
+        <input
+          type="text"
+          placeholder="Написать сообщение..."
+          className={styles.inputField}
+        />
+        <button className={styles.sendButton}>Отправить</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConversationWindow;
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import Header from "../header /header";
 import Sidebar from "../sidebar/sidebar";
 import { MessageProcessor } from "../processor/processor";
+import ConversationWindow from "../conversation_window/conversation_window";
+import styles from "../../styles/home.module.css";
 
 const Home: React.FC = () => {
   return (
     <>
       <MessageProcessor />
       <Header />
-      <Sidebar />
+      <div className={styles.appContainer}>
+        <Sidebar />
+        <ConversationWindow />
+      </div>
     </>
   );
 };
