246b95ebd9825deb8da71ddbb83bbe5440f0cfd1
dz1kill <dz1kill06@gmail.com>
2025-07-10 19:44:26 +0300

    save

---
+++ src/components/sidebar/data.ts
@@ -0,0 +1,37 @@
+export const searchResults = [
+  {
+    id: 1,
+    name: "Алексей Иванов",
+    avatar: "https://example.com/avatars/alexey.jpg",
+    online: true,
+    lastSeen: "online",
+  },
+  {
+    id: 2,
+    name: "Мария Петрова",
+    avatar: "https://example.com/avatars/maria.jpg",
+    online: false,
+    lastSeen: "был(а) 2 часа назад",
+  },
+  {
+    id: 3,
+    name: "Дмитрий Смирнов",
+    avatar: "https://example.com/avatars/dmitry.jpg",
+    online: true,
+    lastSeen: "online",
+  },
+  {
+    id: 4,
+    name: "Екатерина Волкова",
+    avatar: "https://example.com/avatars/ekaterina.jpg",
+    online: false,
+    lastSeen: "был(а) вчера",
+  },
+  {
+    id: 5,
+    name: "Иван Козлов",
+    avatar: "https://example.com/avatars/ivan.jpg",
+    online: true,
+    lastSeen: "online",
+  },
+];
